1db14aba85c38af33abc943ca44d0cdc606c0a77
AdamWeissman <adam@comedytragedyepic.com>
2020-07-18 10:36:20 -0400

    react router setup from documentation

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">HOME</Link>
+            </li>
+            <li>
+              <Link to="/account">ACCOUNT</Link>
+            </li>
+          </ul>
+        </nav>
 
-class App extends React.Component {
-  render() {
-    return (
-      <div> HELLO THERE TEST</div>
-    )
-  };
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/account">
+            <Account />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>HOME</h2>;
+}
+
+function Account() {
+  return <h2>ACCOUNT</h2>;
+}
